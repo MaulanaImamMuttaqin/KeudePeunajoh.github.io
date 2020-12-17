a6aceefbbe140837d4285d451dbf2cbc0548e3ec
Maulana Imam Muttaqin <maulanaimammuttaqin2000@gmail.com>
2020-12-17 17:37:28 +0700

    second finish

--- index.js
+++ index.js
@@ -10,3 +10,16 @@ function removeImage(gambar){
     document.getElementById(gambar).style.animation = "none";
     document.getElementById("main-title").style.animation = "imageShow .3s ease";
 }
+
+// document.getElementById("main-title").addEventListener('mousemove', (e)=>{
+//     console.log(e.clientX, e.clientY)
+// })
+
+
+window.addEventListener("scroll", function(){
+    if(window.scrollY !== 0){
+        document.getElementById("navbar").style.boxShadow= "-1px 1px 13px -5px rgba(0,0,0,0.75)";
+    }else{
+        document.getElementById("navbar").style.boxShadow= "none";
+    }
+  });
